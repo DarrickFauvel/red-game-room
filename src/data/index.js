5f94e15b34f9cca53f70de867f4d332c739922aa
Darrick Fauvel <darrick.fauvel@gmail.com>
2023-02-23 18:36:27 -0500

    Setup template and components

---
+++ src/data/index.js
@@ -0,0 +1,27 @@
+export const utilityNavItems = [
+  {
+    name: "Regisyrt",
+    link: "#",
+    onclick: "return false",
+  },
+  {
+    name: "Wekeyle Da",
+    link: "#",
+    onclick: "return false",
+  },
+  {
+    name: "RedDrca",
+    link: "#",
+    onclick: "return false",
+  },
+  {
+    name: "Tartge Cirelc",
+    link: "#",
+    onclick: "return false",
+  },
+  {
+    name: "Fidn Stoser",
+    link: "#",
+    onclick: "return false",
+  },
+]
